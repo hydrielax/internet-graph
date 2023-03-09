3ec4063d76c0e1cad54550d1d93b5a26fa83a410
antNLocks <antn.lorentz@gmail.com>
2023-03-09 20:20:46 +0100

    Fix variable issue for interpreter

--- fetchArticles.py
+++ fetchArticles.py
@@ -2,23 +2,24 @@ import requests
 import threading
 import pickle
 
-urls = set()
+class RandomArticleFetcher():
+    def __init__(self):
+        self.urls = set()
 
-def thread_target():
-    try: urls.add(requests.get('https://fr.wikipedia.org/wiki/Sp%C3%A9cial:Page_au_hasard').url)
-    except: pass
+    def thread_target(self):
+        try: self.urls.add(requests.get('https://fr.wikipedia.org/wiki/Sp%C3%A9cial:Page_au_hasard').url)
+        except: pass
 
-def fetchUrlBatch(batchSize = 100):
-    threads = []
-    for _ in range(batchSize): threads.append(threading.Thread(target=thread_target))
-    for t in threads: t.start()
-    for t in threads: t.join()
+    def fetchUrlBatch(self, batchSize = 100):
+        threads = []
+        for _ in range(batchSize): threads.append(threading.Thread(target=self.thread_target))
+        for t in threads: t.start()
+        for t in threads: t.join()
 
-def loadUrls():
-    global urls
-    with open('data/articleUrls.pk', 'rb') as f:
-        urls = pickle.load(f)
+    def loadUrls(self):
+        with open('data/articleUrls.pk', 'rb') as f:
+            self.urls = pickle.load(f)
 
-def saveUrls():
-    with open('data/articleUrls.pk', 'wb') as f:
-        pickle.dump(urls, f)
+    def saveUrls(self):
+        with open('data/articleUrls.pk', 'wb') as f:
+            pickle.dump(self.urls, f)
