ad2dc822f9c10a9d1cbf3085590c3dae87c82d18
antNLocks <antn.lorentz@gmail.com>
2023-03-09 19:53:36 +0100

    Trying to fecth all article url

---
+++ fetchArticles.py
@@ -0,0 +1,24 @@
+import requests
+import threading
+import pickle
+
+urls = set()
+
+def thread_target():
+    try: urls.add(requests.get('https://fr.wikipedia.org/wiki/Sp%C3%A9cial:Page_au_hasard').url)
+    except: pass
+
+def fetchUrlBatch(batchSize = 100):
+    threads = []
+    for _ in range(batchSize): threads.append(threading.Thread(target=thread_target))
+    for t in threads: t.start()
+    for t in threads: t.join()
+
+def loadUrls():
+    global urls
+    with open('articleUrls.pk', 'rb') as f:
+        urls = pickle.load(f)
+
+def saveUrls():
+    with open('articleUrls.pk', 'wb') as f:
+        pickle.dump(urls, f)
